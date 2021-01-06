095320d717d095f08c454f501d28b9f05db711c8
Åke Engelbrektson <eson57@users.noreply.github.com>
2021-01-06 16:14:26 +0100

    Create harbour-notizenmd-sv.ts
    Add Swedish translation

---
+++ translations/harbour-notizenmd-sv.ts
@@ -0,0 +1,209 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="sv">
+<context>
+    <name></name>
+    <message id="filemanager-la-folder_no_permission_to_access">
+        <source>No permissions to access %1</source>
+        <translation>Inga åtkomsträttigheter för %1</translation>
+    </message>
+    <message id="filemanager-me-sort">
+        <source>Sort</source>
+        <translation>Sortera</translation>
+    </message>
+    <message id="filemanager-me-new_folder">
+        <source>New folder</source>
+        <translation>Ny mapp</translation>
+    </message>
+    <message id="filemanager-la-deleting">
+        <source>Deleting</source>
+        <translation>Tar bort</translation>
+    </message>
+    <message id="filemanager-la-file_details">
+        <source>%1, %2</source>
+        <extracomment>Shows size and modification date, e.g. &quot;15.5MB, 02/03/2016&quot;</extracomment>
+        <translation>%1, %2</translation>
+    </message>
+    <message id="filemanager-la-no_files">
+        <source>No files</source>
+        <translation>Inga filer</translation>
+    </message>
+    <message id="filemanager-la-operation_already_in_progress">
+        <source>File operation already in progress</source>
+        <translation>Åtgärden pågår redan</translation>
+    </message>
+    <message id="filemanager-la-copy_failed">
+        <source>Copying failed</source>
+        <translation>Kopiering misslyckades</translation>
+    </message>
+    <message id="filemanager-la-deletion_failed">
+        <source>Deletion failed</source>
+        <translation>Borttagning misslyckades</translation>
+    </message>
+    <message id="filemanager-la-moving_failed">
+        <source>Moving failed</source>
+        <translation>Flytt misslyckades</translation>
+    </message>
+    <message id="filemanager-la-renaming_failed">
+        <source>Renaming failed</source>
+        <translation>Namnbyte misslyckades</translation>
+    </message>
+    <message id="filemanager-la-cannot_copy_folder_into_itself">
+        <source>You cannot copy a folder into itself</source>
+        <translation>Du kan inte kopiera en mapp till sig själv</translation>
+    </message>
+    <message id="filemanager-la-copying_folder_failed">
+        <source>Copying folder failed</source>
+        <translation>Mappkopiering misslyckades</translation>
+    </message>
+    <message id="filemanager-la-folder_creation_failed">
+        <source>Could not create folder</source>
+        <translation>Kunde inte skapa mapp</translation>
+    </message>
+    <message id="filemanager-la-set_permissions_failed">
+        <source>Could not set permission</source>
+        <translation>Kunde inte ange rättighet</translation>
+    </message>
+    <message id="filemanager-la-mounting">
+        <source>Mounting SD card</source>
+        <translation>Monterar SD-kort</translation>
+    </message>
+    <message id="filemanager-la-copying">
+        <source>Copying</source>
+        <translation>Kopierar</translation>
+    </message>
+    <message id="filemanager-he-create">
+        <source>Create</source>
+        <translation>Skapa</translation>
+    </message>
+    <message id="filemanager-me-paste">
+        <source>Paste</source>
+        <translation>Klistra in</translation>
+    </message>
+    <message id="filemanager-me-copy">
+        <source>Copy</source>
+        <translation>Kopiera</translation>
+    </message>
+    <message id="filemanager-me-delete">
+        <source>Delete</source>
+        <translation>Ta bort</translation>
+    </message>
+</context>
+<context>
+    <name>About</name>
+    <message>
+        <source>About %1</source>
+        <translation>Om %1</translation>
+    </message>
+    <message>
+        <source>Author</source>
+        <translation>Utvecklare</translation>
+    </message>
+    <message>
+        <source>Source</source>
+        <translation>Källa</translation>
+    </message>
+    <message>
+        <source>Other Projects</source>
+        <translation>Andra projekt</translation>
+    </message>
+</context>
+<context>
+    <name>DirectoryPickerPage</name>
+    <message>
+        <source>Pick (%1)</source>
+        <translation>Välj (%1)</translation>
+    </message>
+    <message>
+        <source>Pick Folder</source>
+        <translation>Välj mapp</translation>
+    </message>
+</context>
+<context>
+    <name>MdViewPage</name>
+    <message>
+        <source>Toggle html/text</source>
+        <translation>Växla html/text</translation>
+    </message>
+    <message>
+        <source>CheatSheet</source>
+        <translation>Fusklapp</translation>
+    </message>
+    <message>
+        <source>Switch to %1</source>
+        <translation>Växla till %1</translation>
+    </message>
+    <message>
+        <source>Edit</source>
+        <translation>Redigera</translation>
+    </message>
+</context>
+<context>
+    <name>NewNoteDialog</name>
+    <message>
+        <source>New note</source>
+        <translation>Ny anteckning</translation>
+    </message>
+    <message>
+        <source>Auto-add extension (.md)</source>
+        <translation>Lägg till filändelse automatiskt (.md)</translation>
+    </message>
+    <message>
+        <source>Edit note after creation.</source>
+        <translation>Redigera anteckning efter skapad.</translation>
+    </message>
+</context>
+<context>
+    <name>NotesBrowser</name>
+    <message>
+        <source>Settings</source>
+        <translation>Inställningar</translation>
+    </message>
+    <message>
+        <source>New Note</source>
+        <translation>Ny anteckning</translation>
+    </message>
+    <message>
+        <source>About %1</source>
+        <translation>Om %1</translation>
+    </message>
+</context>
+<context>
+    <name>Settings</name>
+    <message>
+        <source>About</source>
+        <translation>Om</translation>
+    </message>
+    <message>
+        <source>Settings</source>
+        <translation>Inställningar</translation>
+    </message>
+    <message>
+        <source>Files</source>
+        <translation>Filer</translation>
+    </message>
+    <message>
+        <source>Path to folder containing notes</source>
+        <translation>Sökväg till mapp med anteckningar</translation>
+    </message>
+    <message>
+        <source>Choose Folder</source>
+        <translation>Välj mapp</translation>
+    </message>
+    <message>
+        <source>File filters separated by comma</source>
+        <translation>Filfilter separerade med kommatecken</translation>
+    </message>
+    <message>
+        <source>Select WebView (md by marked.js) or TextArea (md by mistune.py) for viewing notes.</source>
+        <translation>Välj WebView (md av marked.js) eller TextArea (md av mistune.py) för visning av anteckningar.</translation>
+    </message>
+</context>
+<context>
+    <name>harbour-notizenmd</name>
+    <message>
+        <source>Notes</source>
+        <translation>Anteckningar</translation>
+    </message>
+</context>
+</TS>
